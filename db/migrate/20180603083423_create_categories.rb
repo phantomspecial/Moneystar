1cb64dca8a2f5896bd89b31f5f3c54b3f4e4ee5a
phantomspecial <win.2000@live.jp>
2018-06-16 01:45:10 +0900

    category_table_refs

--- db/migrate/20180603083423_create_categories.rb
+++ db/migrate/20180603083423_create_categories.rb
@@ -1,9 +1,10 @@
 class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
+      t.references :top_category, null: false
+      t.references :sub_category, null: false
+      t.references :cf_category, null: false
       t.integer :uuid, null: false
-      t.string :cat_name, null: false
-      t.string :cf_cat, null: false
       t.string :name, null: false
       t.timestamps
     end
