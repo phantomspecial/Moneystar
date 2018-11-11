a5c0a998683f0e6a2d78e4f6e02d816bd6dda6d7
phantomspecial <win.2000@live.jp>
2018-11-12 01:19:35 +0900

    refs #28

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
   root 'masters#index'
 
   resources :masters, only: [:index]
